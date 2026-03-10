138a53179d0b2ac2d50edfbcaa2be4da81f8f156
AlphaRayzz <rayrid007@gmail.com>
2026-03-10 13:43:17 +0530

    feature/uc3

--- OOPSbannerapp.java
+++ OOPSbannerapp.java
@@ -1,20 +1,30 @@
-public class    OOPSbannerapp
-{
-    public static void main(String args[])
-    {
+public class OOPSbannerapp {
+    public static void main(String[] args) {
+     
+        printBannerLine(" **** ", " **** ", " *****", "  **** ");
+        
     
-
-        System.out.println("  ***      ***    ******    ******  ");
-        System.out.println(" ** **    ** **   **    **  **      ");
-        System.out.println("**   **  **   **  **    **  **      ");
-        System.out.println("**   **  **   **  ******     ****** ");
-        System.out.println("**   **  **   **  **              **");
-        System.out.println("**   **  **   **  **              **");
-        System.out.println(" ** **    ** **   **        **    **");
-        System.out.println("  ***      ***    **         ****** ");
-
+        printBannerLine(" *    *", " *    *", " *    ", " *     ");
+        
+  
+        printBannerLine(" *    *", " *    *", " *    ", " *     ");
+        
+       
+        printBannerLine(" *    *", " *    *", " *****", "   **  ");
+        
+   
+        printBannerLine(" *    *", " *    *", " *    ", "     * ");
+        
+      
+        printBannerLine(" *    *", " *    *", " *    ", "    ** ");
+        
+      
+        printBannerLine(" **** ", " **** ", " *    ", " ****  ");
+    }
+    
+    public static void printBannerLine(String O1, String O2, String P, String S) {
+        System.out.println(String.join("  ", O1, O2, P, S));
     }
 }
 
-    
 
